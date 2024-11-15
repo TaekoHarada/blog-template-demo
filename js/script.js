8f179046189b856cbe4be4d8103494babb4c8674
TaekoHarada <taecooo512@gmail.com>
2024-11-15 11:49:44 -0700

    update

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   document.body.insertBefore(toggleButton, nav);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 4.5,
+    loop: true, // 無限ループ
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 5000, // 5秒ごとに自動でスライド
+      disableOnInteraction: false,
+    },
+    speed: 800, // スライドのトランジションの速度を800ミリ秒に設定
+  });
+});
